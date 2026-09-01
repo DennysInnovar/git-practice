7c2bec9f033ec9ded3694ac5757edab97d964d08
Instructor <instructor@example.com>
2026-09-01 13:20:48 +0000

    Agrega función multiply

--- ejercicios/03-historial/app.py
+++ ejercicios/03-historial/app.py
@@ -16,7 +16,12 @@ def subtract(a, b):
     return a - b
 
 
+def multiply(a, b):
+    return a * b
+
+
 if __name__ == "__main__":
     assert add(2, 3) == 5
     assert subtract(5, 3) == 2
+    assert multiply(4, 3) == 12
     print("OK")
