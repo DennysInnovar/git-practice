df09dedff636038f96e834b1a0d245ae8afcfe2b
Instructor <instructor@example.com>
2026-09-01 13:20:57 +0000

    Agrega función divide

--- ejercicios/03-historial/app.py
+++ ejercicios/03-historial/app.py
@@ -21,8 +21,13 @@ def multiply(a, b):
     return a * b
 
 
+def divide(a, b):
+    return a / b
+
+
 if __name__ == "__main__":
     assert add(2, 3) == 5
     assert subtract(5, 3) == 2
     assert multiply(4, 3) == 12
+    assert divide(10, 2) == 5
     print("OK")
